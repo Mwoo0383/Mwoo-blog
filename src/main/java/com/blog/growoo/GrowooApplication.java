20d330ce1666a1fea3dad503399877a98f2f5a17
Minwoo <cap019@naver.com>
2025-04-28 18:36:05 +0900

    초기 설정

---
+++ src/main/java/com/blog/growoo/GrowooApplication.java
@@ -0,0 +1,13 @@
+package com.blog.growoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowooApplication.class, args);
+    }
+
+}
